2a281bf2682c86824f51ac777a73820f4d21797a
JeroCaller <j69m17@gmail.com>
2024-06-07 11:07:23 +0900

    테스트 추가 및 리팩토링

--- tests/calculator-logic-test.js
+++ tests/calculator-logic-test.js
@@ -9,8 +9,12 @@ function testAutoFillParenthesis(expStr, expected, silence) {
     return testTools.testFrame(expStr, calcLogic.autoFillParenthesis, expected, silence);
 }
 
-function testAddWhichParenthesis(expSTr, expected, silence) {
-    return testTools.testFrame(expSTr, calcLogic.addWhichParenthesis, expected, silence);
+function testAddWhichParenthesis(expStr, expected, silence) {
+    return testTools.testFrame(expStr, calcLogic.addWhichParenthesis, expected, silence);
+}
+
+function testReplacePercent(expStr, expected, silence) {
+    return testTools.testFrame(expStr, calcLogic.replacePercent, expected, silence);
 }
 
 /**
@@ -27,28 +31,39 @@ function main(allSilence) {
 
     let syntaxErrorMsg = "잘못된 수식입니다.";
 
-    testTools.countTests(
-        applySilence(testCalcExp)("1+2*6/2", 7),
-        applySilence(testCalcExp)("(1+2)*(6/2)", 9),
-        applySilence(testCalcExp)("-3+5", 2),
-        applySilence(testCalcExp)("-+", syntaxErrorMsg),
-        applySilence(testCalcExp)("50%", syntaxErrorMsg),
-        applySilence(testAutoFillParenthesis)("(((8)))", "(((8)))"),
-        applySilence(testAutoFillParenthesis)("(((1+2))", "(((1+2)))"),
-        applySilence(testAutoFillParenthesis)("(((1+3", "(((1+3)))"),
-        applySilence(testAddWhichParenthesis)("", "("),
-        applySilence(testAddWhichParenthesis)("+", "("),
-        applySilence(testAddWhichParenthesis)("-", "("),
-        applySilence(testAddWhichParenthesis)("/", "("),
-        applySilence(testAddWhichParenthesis)("*", "("),
-        applySilence(testAddWhichParenthesis)("(", "("),
-        applySilence(testAddWhichParenthesis)(")", ""),
-        applySilence(testAddWhichParenthesis)("0", "*("),
-        applySilence(testAddWhichParenthesis)("3+", "("),
-        applySilence(testAddWhichParenthesis)("(3+(20/(9+2))", ")"),
-        applySilence(testAddWhichParenthesis)("(3+(20/(9+2))*2", ")"),
-        applySilence(testAddWhichParenthesis)("(3+(20/(9+2)))*2", "*("),
-    );
+    let testArr = [
+        [testCalcExp, ["1+2*6/2", 7]],
+        [testCalcExp, ["(1+2)*(6/2)", 9]],
+        [testCalcExp, ["-3+5", 2]],
+        [testCalcExp, ["-+", syntaxErrorMsg]],
+        [testCalcExp, ["50%", syntaxErrorMsg]],
+        [testAutoFillParenthesis, ["(((8)))", "(((8)))"]],
+        [testAutoFillParenthesis, ["(((1+2))", "(((1+2)))"]],
+        [testAutoFillParenthesis, ["(((1+3", "(((1+3)))"]],
+        [testAddWhichParenthesis, ["", "("]],
+        [testAddWhichParenthesis, ["+", "("]],
+        [testAddWhichParenthesis, ["-", "("]],
+        [testAddWhichParenthesis, ["/", "("]],
+        [testAddWhichParenthesis, ["*", "("]],
+        [testAddWhichParenthesis, ["(", "("]],
+        [testAddWhichParenthesis, [")", ""]],
+        [testAddWhichParenthesis, ["0", "*("]],
+        [testAddWhichParenthesis, ["3+", "("]],
+        [testAddWhichParenthesis, ["(3+(20/(9+2))", ")"]],
+        [testAddWhichParenthesis, ["(3+(20/(9+2))*2", ")"]],
+        [testAddWhichParenthesis, ["(3+(20/(9+2)))*2", "*("]],
+        [testReplacePercent, ["100+20", "100+20"]],
+        [testReplacePercent, ["100%", "100%"]],
+        [testReplacePercent, ["100+20%", "100+20"]],
+        [testReplacePercent, ["200+20%", "200+40"]],
+        [testReplacePercent, ["200+30%+300+10%", "200+60+300+30"]],
+        [testReplacePercent, ["(30+30%)+(20+40%)", "(30+9)+(20+8)"]],
+    ];
+    let appliedTests = [];
+    for (let record of testArr) {
+        appliedTests.push(applySilence(record[0])(...record[1]));
+    }
+    testTools.countTests(...appliedTests);
 }
 
-main(true);
+main();
