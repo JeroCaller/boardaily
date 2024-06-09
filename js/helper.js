1940a38d35423fe4d0c13e9795559d88d82e0cd4
JeroCaller <j69m17@gmail.com>
2024-06-10 08:55:17 +0900

    add docstring

--- js/helper.js
+++ js/helper.js
@@ -2,6 +2,11 @@ export async function getToolInfoInJson() {
     return await fetch('/tools-info.json').then(res => res.json());
 }
 
+/**
+ * 
+ * @param {string} filePath - /images/calculator.png 형태의 파일경로.
+ * @returns - 파일 이름.
+ */
 export function extractFileName(filePath) {
     return filePath.split('/')[2].split('.')[0];
 }
