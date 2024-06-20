c92461183bbe2c1940e0562700cb936b38e2c8f9
JeroCaller <j69m17@gmail.com>
2024-06-20 19:17:31 +0900

    시간 표시 시스템 정보 웹 스토리지에 저장 및 불러오기 기능 추가

--- js/table-page/watch/watch.js
+++ js/table-page/watch/watch.js
@@ -1,5 +1,4 @@
 import * as wLogic from './watch-logic.js';
-import * as helper from '../../helper.js';
 
 class DigitalWatch extends HTMLElement {
     constructor() {
@@ -10,7 +9,7 @@ class DigitalWatch extends HTMLElement {
     async connectedCallback() {
         this.attachShadow({mode: 'open'}).innerHTML = await this.combineStyleAndHTML();
         this.initElement();
-        this.tick();
+        this.init();
         this.setEventHandlers();
     }
 
@@ -38,26 +37,47 @@ class DigitalWatch extends HTMLElement {
         this.hour1224 = this.watchConfig.querySelector('#hours');
     }
 
+    init() {
+        if ('hour-system' in localStorage) {
+            this.hour1224.textContent = localStorage.getItem('hour-system');
+            switch (this.hour1224.textContent) {
+                case '24':
+                    this.tick({hour12: true});
+                    break;
+                case 'AM/PM':
+                    this.tick({hour12: false});
+                    break;
+            }
+        } else {
+            this.tick();
+        }
+    }
+
     tick(datetimeOption) {
         let thisTime = new Date();
         this.dateElement.textContent = `${thisTime.toLocaleDateString()} ${wLogic.getWeekName(thisTime.getDay())}`;
         this.timeElement.textContent = thisTime.toLocaleTimeString('ko-KR', datetimeOption? datetimeOption : undefined);
-        this.watchId = setTimeout(this.tick.bind(this), 1000, datetimeOption);
+        this.watchId = setTimeout(this.tick.bind(this, datetimeOption), 1000);
+    }
+
+    changeHourSystem(textData) {
+        switch (textData) {
+            case '24':
+                this.hour1224.textContent = 'AM/PM';
+                this.tick({hour12: false});
+                break;
+            case 'AM/PM':
+                this.hour1224.textContent = '24';
+                this.tick({hour12: true});
+                break;
+        }
     }
 
     setEventHandlers() {
         this.hour1224.addEventListener('click', () => {
             clearTimeout(this.watchId);
-            switch (this.hour1224.textContent) {
-                case '24':
-                    this.hour1224.textContent = 'AM/PM';
-                    this.tick({hour12: false});
-                    break;
-                case 'AM/PM':
-                    this.hour1224.textContent = '24';
-                    this.tick({hour12: true});
-                    break;
-            }
+            this.changeHourSystem(this.hour1224.textContent);
+            localStorage.setItem('hour-system', this.hour1224.textContent);
         });
     }
 }
