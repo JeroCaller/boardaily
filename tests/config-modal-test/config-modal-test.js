ec6708e78d88699e87a9a4f1ea74a5e4fb925101
JeroCaller <j69m17@gmail.com>
2024-06-21 23:03:18 +0900

    설정 모달 창 초안 구현 및 브라우저 환경 테스트 구현

---
+++ tests/config-modal-test/config-modal-test.js
@@ -0,0 +1,14 @@
+import { createConfigModalElement } from "/js/config-ui/config-modal.js";
+
+function main() {
+    const configModalElement = createConfigModalElement(['z-index', 10]);
+    const bodyElement = document.querySelector('body');
+
+    bodyElement.append(configModalElement);
+
+    document.querySelector('#config-icon').addEventListener('click', () => {
+        configModalElement.style.display = 'flex';
+    });
+}
+
+main();
